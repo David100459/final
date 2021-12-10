ebd096e35e09f553e01dfcbba3562c75d76aca19
David100459 <davidalberto401@gmail.com>
2021-12-10 13:07:18 -0500

    actividad final

--- academic/academic/settings.py
+++ academic/academic/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'academic.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'academic.sqlite3',
+        'NAME': BASE_DIR / 'academic.db',
     }
 }
 
